82ad5a1b31b50bc3ecf9f40ba215db1b99585bbd
Krzy221 <79155912+Krzy221@users.noreply.github.com>
2021-02-24 18:04:25 +0100

    first commit

---
+++ myfirst/src/main/java/com/myfirst/myfirst/PostController.java
@@ -0,0 +1,23 @@
+package com.myfirst.myfirst;
+
+import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+  @GetMapping("/hello")
+  public String Hello() {
+    return "Hello world";
+  }
+  @GetMapping("/addPost")
+  public Post[] AddPost(String title, String content) throws IOException {
+    return JsonActions.addPost(title,content);
+  }
+}
+  
+
+
+
+
+
